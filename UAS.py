91186d3b05df7031b1dd4f4c3ca8934ad1811afc
itsrifkii <38483857+itsrifkii@users.noreply.github.com>
2018-04-18 03:57:31 +0000

    Add files via upload

---
+++ UAS.py
@@ -0,0 +1,18 @@
+import Rifki.ifelse
+import Rifki.random
+import Rifki.append
+import Rifki.tupple
+import Rifki.dict
+import Rifki.opp
+
+print (Rifki.ifelse.fa())
+
+print (Rifki.random.fb())
+
+print (Rifki.append.fc())
+
+print (Rifki.tupple.fd())
+
+print (Rifki.dict.fe())
+
+print (Rifki.opp.ff())
